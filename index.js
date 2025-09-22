25f6699fb1fcab20a1bdf0b1cbbe623a8be6241b
Subhash Bohra <subhash.bohra@gmail.com>
2025-09-22 19:48:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const _ = require('lodash');
+const bodyParser = require('body-parser');
+const sqlRoutes = require('./routes/sql');
+const execRoutes = require('./routes/exec');
+const fileRoutes = require('./routes/fileupload');
+const authRoutes = require('./routes/auth');
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Example vulnerable code: unsanitized input
-app.get('/greet', (req, res) => {
-  const name = req.query.name || "World";
-  res.send("Hello " + name);
-});
+app.use('/sql', sqlRoutes);
+app.use('/exec', execRoutes);
+app.use('/upload', fileRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(port, () => {
-  console.log(`Sample app listening at http://localhost:${port}`);
-});
+app.get('/', (req, res) => res.send('Vuln Testbed Running'));
+app.listen(3000, () => console.log('Listening on 3000'));
